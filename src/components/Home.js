c30c44ea3f4ac16df3b65d94920f89d85880dca6
AimableHonorable <aimatwiri@gmail.com>
2021-07-11 20:47:52 +0200

    useEffect dependencies and error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,10 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
-    {
-      title: "Learn react",
-      author: "mario",
-      id: 1
-    },
-    {
-      title: "my website",
-      author: "lucy",
-      id: 2
-    },
-    {
-      title: "react native tutorial",
-      author: "claire",
-      id: 3
-    }
-  ])
+  const [blogs, setBlog] = useState(null)
+
+  // initially set the loading state to true in order to show the loading message on the screen while fetching data from the server. 
+  const [isPending, setisPending] = useState(true)
 
   const handleDelete = (id) => {
     setBlog(blogs.filter((blog) => blog.id !== id))
@@ -27,15 +14,25 @@ const Home = () => {
 
   // useEffect runs everytime a render occurs
   useEffect(() => {
-    console.log("use effect");
-  })
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlog(data);
+
+      // after fetching the data we need to stop showing loading message to the screen by setting isPending function state to false
+      setisPending(false);
+    })
+  }, [])
 
   return (
     <div>
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      { isPending && <div><h2>Loading, Please wait...</h2></div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
 
       {/* filter list items according to various conditions */}
-      <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's blogs" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's blogs" handleDelete={handleDelete} />}
     </div>
   )
 }
