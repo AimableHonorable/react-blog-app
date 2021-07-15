1dbea792e9437e32156c3385c64d138ee27e4061
AimableHonorable <aimatwiri@gmail.com>
2021-07-15 13:40:18 +0200

    added details page, create funhctionh anhd delete

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import useFetch from "../useFetch"
 
 const BlogDetails = () => {
   const {id} = useParams()
   const {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
+  const history = useHistory()
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/ ')
+    })
+  }
   return (
     <div className="blog-details">
       { isPending && <div className="loader"></div> }
@@ -11,7 +19,9 @@ const BlogDetails = () => {
       {blog && (
         <article>
           <h2>{blog.title}</h2>
-          <p>{blog.author}</p>
+          <p>Written by <strong> {blog.author}</strong></p>
+          <p>{blog.body}</p>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [body, setBody] = useState('')
+  const history = useHistory()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    const blog = {title, author, body}
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify(blog)
+      
+    }).then(() => {
+      history.push('/')
+    })
+  }
+
   return (
     <div className="blog-details">
       <h2>New blog</h2>
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td><label>Blog title:</label></td>
+              <td>
+                <input 
+                  type="text" 
+                  required 
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Blog Author:</label>
+              </td>
+              <td>
+                <input 
+                  type="text" 
+                  required
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Blog Body:</label>
+              </td>
+              <td>
+                <textarea 
+                  cols="30" 
+                  rows="10" 
+                  required
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button>Add blog</button>
+              </td>
+            </tr>
+          </tbody>
+          
+        </table>
+      </form>
+      <div>
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <p>{body}</p>
+      </div>
     </div>
   )
 }
